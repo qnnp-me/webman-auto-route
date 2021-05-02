00688fa3264d3b667bd2bac5354cddcc91ebe924
pfan <wjp@pfan.ltd>
2021-05-02 14:57:53 +0800

    增加对象自动提示能力，OpenAPI对应的节点都有对象提示、友好性更高

--- src/Controller/Swagger.php
+++ src/Controller/Swagger.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * This file is part of webman-auto-route.
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author    qnnp<qnnp@qnnp.me>
+ * @copyright qnnp<qnnp@qnnp.me>
+ * @link      https://main.qnnp.me
+ * @license   https://opensource.org/licenses/MIT MIT License
+ */
 
 namespace WebmanPress\AutoRoute\Controller;
 
